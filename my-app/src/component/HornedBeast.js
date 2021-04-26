070ea8dd98b991ce4f9f17fff990701b22fcfb49
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-04-27 01:27:38 +0300

    stayl

--- my-app/src/component/HornedBeast.js
+++ my-app/src/component/HornedBeast.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 
 class HornedBeast extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.src } alt=''/>
-                <p>{this.props.description}</p>
+    constructor(props){
+        super(props);
+        this.state={
+            rate:0
+        };
+
+        }
+        addRate=()=>{
+            this.setState({
+                rate:this.state.rate+1
+            });
+        }
+    
+        render (){
+            return (
+             <Card style={{ width: '30rem',margin:'3rem 37rem 3rem 37rem', color:'#0064ED' , textAlign:'center', border:'2px solid #00ADBE'}}>
+             <Card.Img  src={this.props.image_url} />
+             <Card.Body>
+             <Card.Title>{this.props.title}</Card.Title>
+             <Card.Text>
+             {this.props.description}
+             </Card.Text>
+             <Card.Text>💙
+             {this.state.rate}
+             </Card.Text>
+             <Button onClick={this.addRate}variant="primary">Rate</Button>
+             </Card.Body>
+             </Card>
+             );  
+         }
+     }
 
 
-                
-
-            </div>
-            
-           
-        )
-    };
-}
 
 export default HornedBeast;
--- my-app/src/component/header.js
+++ my-app/src/component/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 class Header extends React.Component{
     render(){
         return(
-            <h1>Rhino Family</h1>
+            <h1>Gallery or Horn</h1>
         )
     };
 }
