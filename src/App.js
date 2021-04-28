9b41050f6322736af301dea67b6b25ecd23db0a9
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-04-28 22:20:28 +0300

    model2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import './App.css';
 import React from 'react';
+import DataJson from './assest/data.json';
 import Main from './component/main';
 import Header from './component/header';
 import Footer from './component/footer';
+import SelectedBeast from './component/SelectedBeast';
+
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: DataJson,
+      show: false,
+      info: {}
+    };
+  }
+  viewBeast = (beast) => {
+    this.setState({
+      show: true,
+      info: beast
+    });
+  }
+  closing = () => {
+    this.setState({
+      show: false,
+      info: {}
+    });
+  }
+
   render() {
     return (
       <div>
-        <Header/>
-        <Main/>
+        <Header />
+        <Main data={this.state.data} viewBeast={this.viewBeast} />
+        <SelectedBeast show={this.state.show}
+          info={this.state.info}
+          closing={this.closing}/>
         <Footer />
       </div>
     );
